a494a685ffa6a03d9504c703cfe15a4d2b1a3b48
Christian Flothmann <christian.flothmann@uos.de>
2014-10-06 15:33:00 +0200

    add a database table to store meeting for a course

---
+++ migrations/003_create_meetings_table.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Creates a table that maps meeting ids to courses.
+ *
+ * A meeting can be identified by both an integer identifier as well as as
+ * string based unique id.
+ *
+ * @author Christian Flothmann <christian.flothmann@uos.de>
+ */
+class CreateMeetingsTable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function description()
+    {
+        return 'Creates a table that maps meeting ids to courses.';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        DBManager::get()->exec(
+            'CREATE TABLE IF NOT EXISTS vc_meetings (
+              id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+              identifier VARCHAR(32) NOT NULL,
+              remote_id VARCHAR(32),
+              course_id VARCHAR(32) NOT NULL,
+              name VARCHAR(255) NOT NULL,
+              driver VARCHAR(32) NOT NULL,
+              attendee_password VARCHAR(32),
+              moderator_password VARCHAR(32),
+              UNIQUE KEY(identifier)
+            )'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        DBManager::get()->exec('DROP TABLE IF EXISTS vc_meetings');
+    }
+}
