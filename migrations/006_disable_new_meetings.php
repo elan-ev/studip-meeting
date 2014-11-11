8f68fc7b1c2b8c19a37160ebc099548309271342
Christian Flothmann <christian.flothmann@uos.de>
2014-11-11 17:01:56 +0100

    don't enable meetings by default

---
+++ migrations/006_disable_new_meetings.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Sets the default value of the active property to false.
+ *
+ * @author Christian Flothmann <christian.flothmann@uos.de>
+ */
+class DisableNewMeetings extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function description()
+    {
+        return 'Sets the default value of the active property to false';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $db = DBManager::get();
+        $db->exec(
+            'ALTER TABLE
+              vc_meetings
+            MODIFY active TINYINT NOT NULL DEFAULT 0'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $db = DBManager::get();
+        $db->exec(
+            'ALTER TABLE
+              vc_meetings
+            MODIFY active TINYINT NOT NULL DEFAULT 1'
+        );
+    }
+}
