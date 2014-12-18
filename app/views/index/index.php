9d9401ad9c4f25fa34344985cf1ed99db94377b0
Christian Flothmann <christian.flothmann@uos.de>
2014-12-18 16:12:57 +0100

    tweak meeting list

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -29,9 +29,8 @@
     <? endif ?>
 
     <div>
-        <h1>Konferenzen</h1>
-
         <table class="default collapsable tablesorter conference-meetings">
+            <caption><?=_('Konferenzen')?></caption>
             <colgroup>
                 <col>
                 <col style="width: 100px;">
@@ -46,77 +45,86 @@
                 <th><?=_('Aktion')?></th>
             </tr>
             </thead>
-        <?php foreach ($meetings as $meeting): ?>
-            <?php
-            $joinUrl = PluginEngine::getLink($plugin, array(), 'index/joinMeeting/'.$meeting->id);
-            $moderatorPermissionsUrl = PluginEngine::getLink($plugin, array(), 'index/moderator_permissions/'.$meeting->id);
-            $deleteUrl = PluginEngine::getLink($plugin, array('delete' => $meeting->id), 'index');
-            ?>
-            <tr>
-                <td class="meeting-name">
-                    <img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/grey/info-circle.png" class="info">
-                    <a href="<?=$joinUrl?>" title="<?=_('Meeting betreten')?>" target="_blank"><?=htmlReady($meeting->name)?></a>
-                    <input type="text" name="name">
-                    <img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/grey/accept.png" class="accept-button" title="<?=_('Änderungen speichern')?>">
-                    <img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/grey/decline.png" class="decline-button" title="<?=_('Änderungen verwerfen')?>">
-                    <img src="<?=$GLOBALS['ASSETS_URL']?>/images/ajax_indicator_small.gif" class="loading-indicator">
 
-                    <div class="info">
-                        <?php if ($meeting->join_as_moderator): ?>
-                            <?=_('Alle Teilnehmenden haben Moderationsrechte.')?>
-                        <?php else: ?>
-                            <?=_('Nur DozentInnen und TutorInnen haben Moderationsrechte.')?>
+            <tbody>
+                <?php foreach ($meetings as $meeting): ?>
+                    <?php
+                    $joinUrl = PluginEngine::getLink($plugin, array(), 'index/joinMeeting/'.$meeting->id);
+                    $moderatorPermissionsUrl = PluginEngine::getLink($plugin, array(), 'index/moderator_permissions/'.$meeting->id);
+                    $deleteUrl = PluginEngine::getLink($plugin, array('delete' => $meeting->id), 'index');
+                    ?>
+                    <tr>
+                        <td class="meeting-name">
+                            <img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/grey/info-circle.png" class="info">
+                            <a href="<?=$joinUrl?>" title="<?=_('Meeting betreten')?>" target="_blank"><?=htmlReady($meeting->name)?></a>
+                            <input type="text" name="name">
+                            <img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/grey/accept.png" class="accept-button" title="<?=_('Änderungen speichern')?>">
+                            <img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/grey/decline.png" class="decline-button" title="<?=_('Änderungen verwerfen')?>">
+                            <img src="<?=$GLOBALS['ASSETS_URL']?>/images/ajax_indicator_small.gif" class="loading-indicator">
+
+                            <div class="info">
+                                <?php if ($meeting->join_as_moderator): ?>
+                                    <?=_('Alle Teilnehmenden haben Moderationsrechte.')?>
+                                <?php else: ?>
+                                    <?=_('Nur DozentInnen und TutorInnen haben Moderationsrechte.')?>
+                                <?php endif; ?>
+
+                                <br>
+
+                                <?php if (count($meeting->getRecentJoins()) === 1): ?>
+                                    <?=_('Eine Person hat das Meeting in den letzten 24 Stunden betreten')?>.
+                                <?php else: ?>
+                                    <?=count($meeting->getRecentJoins()).' '._('Personen haben das Meeting in den letzten 24 Stunden betreten')?>.
+                                <?php endif; ?>
+
+                                <br>
+
+                                <?php if (count($meeting->getAllJoins()) === 1): ?>
+                                    <?=_('Eine Person hat das Meeting insgesamt betreten')?>.
+                                <?php else: ?>
+                                    <?=count($meeting->getAllJoins()).' '._('Personen haben das Meeting insgesamt betreten')?>.
+                                <?php endif; ?>
+                            </div>
+                        </td>
+                        <?php if ($canModifyCourse): ?>
+                            <td><input type="checkbox"<?=$meeting->active ? ' checked="checked"' : ''?> data-meeting-enable-url="<?=PluginEngine::getLink($plugin, array(), 'index/enable/'.$meeting->id)?>" title="<?=$meeting->active ? _('Meeting für Studierende unsichtbar schalten') : _('Meeting für Studierende sichtbar schalten')?>"></td>
                         <?php endif; ?>
+                        <td>
+                            <?php if ($canModifyCourse): ?>
+                                <a href="#" title="<?=_('Meeting umbenennen')?>" class="edit-meeting" data-meeting-rename-url="<?=PluginEngine::getLink($plugin, array(), 'index/rename/'.$meeting->id)?>"><img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/blue/edit.png"></a>
+                                <?php if ($meeting->join_as_moderator): ?>
+                                    <a href="<?=$moderatorPermissionsUrl?>" title="<?=_('Ändern zu: nur DozentInnen und TutorInnen haben Moderationsrechte')?>"><img src="<?=$plugin->getAssetsUrl()?>/images/moderator-enabled.png"></a>
+                                <?php else: ?>
+                                    <a href="<?=$moderatorPermissionsUrl?>" title="<?=_('Ändern zu: alle Teilnehmenden haben Moderationsrechte')?>"><img src="<?=$plugin->getAssetsUrl()?>/images/moderator-disabled.png"></a>
+                                <?php endif; ?>
+                                <a href="<?=$deleteUrl?>" title="<?=_('Meeting löschen')?>"><img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/blue/trash.png"></a>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
 
-                        <br>
-
-                        <?php if (count($meeting->getRecentJoins()) === 1): ?>
-                            <?=_('Eine Person hat das Meeting in den letzten 24 Stunden betreten')?>.
-                        <?php else: ?>
-                            <?=count($meeting->getRecentJoins()).' '._('Personen haben das Meeting in den letzten 24 Stunden betreten')?>.
-                        <?php endif; ?>
-
-                        <br>
-
-                        <?php if (count($meeting->getAllJoins()) === 1): ?>
-                            <?=_('Eine Person hat das Meeting insgesamt betreten')?>.
-                        <?php else: ?>
-                            <?=count($meeting->getAllJoins()).' '._('Personen haben das Meeting insgesamt betreten')?>.
-                        <?php endif; ?>
-                    </div>
-                </td>
-                <?php if ($canModifyCourse): ?>
-                    <td><input type="checkbox"<?=$meeting->active ? ' checked="checked"' : ''?> data-meeting-enable-url="<?=PluginEngine::getLink($plugin, array(), 'index/enable/'.$meeting->id)?>" title="<?=$meeting->active ? _('Meeting für Studierende unsichtbar schalten') : _('Meeting für Studierende sichtbar schalten')?>"></td>
-                <?php endif; ?>
-                <td>
-                    <?php if ($canModifyCourse): ?>
-                        <a href="#" title="<?=_('Meeting umbenennen')?>" class="edit-meeting" data-meeting-rename-url="<?=PluginEngine::getLink($plugin, array(), 'index/rename/'.$meeting->id)?>"><img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/blue/edit.png"></a>
-                        <?php if ($meeting->join_as_moderator): ?>
-                            <a href="<?=$moderatorPermissionsUrl?>" title="<?=_('Ändern zu: nur DozentInnen und TutorInnen haben Moderationsrechte')?>"><img src="<?=$plugin->getAssetsUrl()?>/images/moderator-enabled.png"></a>
-                        <?php else: ?>
-                            <a href="<?=$moderatorPermissionsUrl?>" title="<?=_('Ändern zu: alle Teilnehmenden haben Moderationsrechte')?>"><img src="<?=$plugin->getAssetsUrl()?>/images/moderator-disabled.png"></a>
-                        <?php endif; ?>
-                        <a href="<?=$deleteUrl?>" title="<?=_('Meeting löschen')?>"><img src="<?=$GLOBALS['ASSETS_URL']?>/images/icons/20/blue/trash.png"></a>
-                    <?php endif; ?>
-                </td>
-            </tr>
-            <?php endforeach; ?>
+            <?php if ($canModifyCourse): ?>
+                <tfoot>
+                    <tr>
+                        <td colspan="<?=$canModifyCourse ? 3 : 2?>">
+                            <form method="post" action="<?=PluginEngine::getURL($GLOBALS['plugin'], array(), 'index')?>" class="create-conference-meeting">
+                                <fieldset name="Meeting erstellen">
+                                    <?php if (count($errors) > 0): ?>
+                                        <ul>
+                                            <?php foreach ($errors as $error): ?>
+                                                <li><?php echo htmlReady($error); ?></li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                    <?php endif; ?>
+                                    <input type="text" name="name" placeholder="">
+                                    <input type="submit" value="Meeting erstellen">
+                                </fieldset>
+                            </form>
+                        </td>
+                    </tr>
+                </tfoot>
+            <?php endif; ?>
         </table>
-
-        <?php if ($canModifyCourse): ?>
-        <form method="post" action="<?=PluginEngine::getURL($GLOBALS['plugin'], array(), 'index')?>" class="create-conference-meeting">
-            <fieldset name="Meeting erstellen">
-                <?php if (count($errors) > 0): ?>
-                    <ul>
-                        <?php foreach ($errors as $error): ?>
-                            <li><?php echo htmlReady($error); ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php endif; ?>
-                <input type="text" name="name" placeholder="">
-                <input type="submit" value="Meeting erstellen">
-            </fieldset>
-        </form>
-        <?php endif; ?>
     </div>
 <? endif ?>
