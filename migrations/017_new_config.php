db9b1d5a1e12bcc475fa06946be9591cc93f4f87
tgloeggl <tgloeggl@uos.de>
2015-10-05 12:27:03 +0200

    fix migration and keep old settings

--- migrations/017_new_config.php
+++ migrations/017_new_config.php
@@ -15,7 +15,7 @@ class NewConfig extends Migration {
      */
     public function description()
     {
-        return "remove old config options and add the new one";
+        return "migrate and remove old config options and add the new one";
     }
 
     /**
@@ -26,14 +26,34 @@ class NewConfig extends Migration {
         try {
             Config::get()->create('VC_CONFIG');
 
-            // TODO: migrate current settings
+            //migrate current settings
+            $current_driver = Config::get()->getValue('VC_DRIVER');
+
+            $config = array(
+                'BigBlueButton' => array(
+                    'enable'       => ($current_driver == 'bigbluebutton') ? 1 : 0,
+                    'display_name' => 'BigBlueButton',
+                    'url'          => Config::get()->getValue('BBB_URL'),
+                    'api-key'      => Config::get()->getValue('BBB_SALT')
+                ),
+                'DfnVc' => array(
+                    'enable' => ($current_driver == 'dfnvc') ? 1 : 0,
+                    'display_name' => 'Adobe Connect VC',
+                    'url'          => Config::get()->getValue('DFN_VC_URL'),
+                    'login'        => Config::get()->getValue('DFN_VC_LOGIN'),
+                    'password'     => Config::get()->getValue('DFN_VC_PASSWORD')
+                )
+            );
+            \Config::get()->store('VC_CONFIG', json_encode($config));
 
             Config::get()->delete('BBB_URL');
             Config::get()->delete('BBB_SALT');
             Config::get()->delete('DFN_VC_URL');
             Config::get()->delete('DFN_VC_LOGIN');
             Config::get()->delete('DFN_VC_PASSWORD');
+            Config::get()->delete('VC_DRIVER');
             Config::get()->delete(\ElanEv\Driver\DriverFactory::DEFAULT_DRIVER_CONFIG_ID);
+
         } catch (InvalidArgumentException $ex) {
 
         }
