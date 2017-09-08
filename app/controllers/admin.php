8ccb32e8034956e30cae0deda1392b1c1b130000
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-09-08 15:38:22 +0200

    adjust translations, re #3

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -27,12 +27,59 @@ use ElanEv\Model\Driver;
  * @property array                  $errors
  * @property string                 $deleteAction
  */
-class AdminController extends UOL\StudipController
+class AdminController extends StudipController
 {
     /**
      * @var ElanEv\Driver\DriverInterface
      */
     private $driver;
+
+    /**
+     * Constructs the controller and provide translations methods.
+     *
+     * @param object $dispatcher
+     * @see https://stackoverflow.com/a/12583603/982902 if you need to overwrite
+     *      the constructor of the controller
+     */
+    public function __construct($dispatcher)
+    {
+        parent::__construct($dispatcher);
+
+        $this->plugin = $dispatcher->current_plugin;
+
+        // Localization
+        $this->_ = function ($string) use ($dispatcher) {
+            return call_user_func_array(
+                [$dispatcher->current_plugin, '_'],
+                func_get_args()
+            );
+        };
+
+        $this->_n = function ($string0, $tring1, $n) use ($dispatcher) {
+            return call_user_func_array(
+                [$dispatcher->plugin, '_n'],
+                func_get_args()
+            );
+        };
+    }
+
+    /**
+     * Intercepts all non-resolvable method calls in order to correctly handle
+     * calls to _ and _n.
+     *
+     * @param string $method
+     * @param array  $arguments
+     * @return mixed
+     * @throws RuntimeException when method is not found
+     */
+    public function __call($method, $arguments)
+    {
+        $variables = get_object_vars($this);
+        if (isset($variables[$method]) && is_callable($variables[$method])) {
+            return call_user_func_array($variables[$method], $arguments);
+        }
+        throw new RuntimeException("Method {$method} does not exist");
+    }
 
     /**
      * {@inheritdoc}
