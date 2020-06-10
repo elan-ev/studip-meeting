ee6763ea2b30ae580a3619deb7a6ea5eaa2c27ee
farbod <zamanifarbod2@gmail.com>
2020-06-10 12:35:28 +0200

    reading the currect opencast series id

--- lib/Routes/Rooms/RoomEdit.php
+++ lib/Routes/Rooms/RoomEdit.php
@@ -63,6 +63,7 @@ class RoomEdit extends MeetingsController
             !isset($json['server_index']) ?: $meeting->server_index = $json['server_index'];
 
             // apply default features
+
             if (isset($json['features'])) {
                 if (!is_numeric($json['features']['duration'])) {
                     $json['features']['duration'] = "240";
@@ -76,8 +77,8 @@ class RoomEdit extends MeetingsController
                         $record = 'true';
                         if (Driver::getConfigValueByDriver($json['driver_name'], 'opencast')) { // config check for opencast
                             $series_id = MeetingPlugin::checkOpenCast($json['cid']);
-                            if ($series_id && is_array($series_id)) {
-                                $opencast_series_id = $series_id[0];
+                            if ($series_id) {
+                                $opencast_series_id = $series_id;
                             } else {
                                 $message = [
                                     'text' => _('Opencast Series id kann nicht gefunden werden!'),
