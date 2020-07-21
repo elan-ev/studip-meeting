da98cf692d3ae9d99a42dac5d91a1b8118907ae3
farbod <zamanifarbod2@gmail.com>
2020-07-21 09:52:36 +0200

    input number correction,  fixes issue #98

--- vueapp/views/Course.vue
+++ vueapp/views/Course.vue
@@ -134,7 +134,7 @@
                                 {{ feature['display_name'] | i18n }}
                                 <StudipTooltipIcon v-if="Object.keys(feature).includes('info')" :text="feature['info'] | i18n"></StudipTooltipIcon>
 
-                                <input type="text" v-model.trim="room['features'][feature['name']]" :placeholder="feature['value'] ? feature['value'] : ''" :id="feature['name']">
+                                <input :type="(feature['name'] == 'duration' || feature['name'] == 'maxParticipants') ? 'number' : 'text'" v-model.trim="room['features'][feature['name']]" :placeholder="feature['value'] ? feature['value'] : ''" :id="feature['name']">
                             </label>
                         </div>
                     </div>
