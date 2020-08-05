edf87109eeb0864be5191420e365c7bfcb0238cb
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2020-08-05 11:43:22 +0200

    Delete index.php

--- app/views/admin/index.php
+++
@@ -1,8 +0,0 @@
-<div class="container" id="app">
-    <h1 class="display-1 text-center">Starte Anwendung&hellip;</h1>
-</div>
-
-<script type="text/javascript">
-    let API_URL  = '<?= PluginEngine::getURL('meetingplugin', [], 'api') ?>';
-    let ICON_URL = '<?= Assets::url('images/icons/') ?>';
-</script>
