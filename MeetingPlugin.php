f4f84ddb01d501bfdff390beae440c6c0a352845
Jakob Diel <jakob.diel@hs-wismar.de>
2020-12-11 12:29:41 +0100

    übersetzung der plugin metadaten

--- MeetingPlugin.php
+++ MeetingPlugin.php
@@ -351,4 +351,13 @@ class MeetingPlugin extends StudIPPlugin implements StandardPlugin, SystemPlugin
         }
         return false;
     }
+    function getMetadata() {
+        $metadata = parent::getMetadata();
+        $metadata['pluginname'] = _("Meetings");
+        $metadata['displayname'] = _("Meetings");
+        $metadata['descriptionlong'] = _("Virtueller Raum, mit dem Live-Online-Treffen, Veranstaltungen und Videokonferenzen durchgeführt werden können. Die Teilnehmenden können sich während eines Meetings gegenseitig hören und über eine angeschlossene Webcam - wenn vorhanden - sehen und miteinander arbeiten. Folien können präsentiert und Abfragen durchgeführt werden. Ein Fenster in der Benutzungsoberfläche des eigenen Rechners kann für andere sichtbar geschaltet werden, um zum Beispiel den Teilnehmenden bestimmte Webseiten oder Anwendungen zu zeigen. Außerdem kann die Veranstaltung aufgezeichnet und Interessierten zur Verfügung gestellt werden.");
+        $metadata['descriptionshort'] = _("Face-to-face-Kommunikation mit Adobe Connect oder BigBlueButton");
+        $metadata['keywords'] = _("Videokonferenz- und Veranstaltungsmöglichkeit;Live im Netz präsentieren sowie gemeinsam zeichnen und arbeiten;Kommunikation über Mikrofon und Kamera;Ideal für dezentrale Lern- und Arbeitsgruppen;Verlinkung zu bereits bestehenden eigenen Räumen");
+        return $metadata;
+    }
 }
