5247433a73df1cbc6d124da05243c13cd5b7cebe
farbod <zamanifarbod2@gmail.com>
2022-04-11 16:46:32 +0200

    optimize the default guest name display, fixes #364

--- app/views/room/index.php
+++ app/views/room/index.php
@@ -1,8 +1,13 @@
 <form method="post" action="<?= $controller->url_for('room/join_meeting/' . $invitations_link->hex . "/$cid")?>" class="default">
-    <h1><?= $_('Sie wurden zur Teilnahme eingeladen')?></h1>
+    <h1><?= $_('Sie wurden zur Teilnahme eingeladen') ?></h1>
     <label>
-        <?= $_('Geben Sie einen Namen ein')?>
-        <input type="text" name="name" value="" placeholder="<?= htmlReady($invitations_link->default_name)?>">
+        <?= $_('Geben Sie einen Namen ein') ?>
+        <input type="text" name="name" value="" autofocus>
+    </label>
+    <label>
+        <?= $_('Oder verwenden Sie den Standard-Gästename') ?>
+        <?= tooltipIcon(_('Sofern Sie keinen Namen eingeben, wird dieser standardmäßig verwendet.')) ?>
+        <input type="text" value="<?= htmlReady($invitations_link->default_name) ?>" readonly>
     </label>
     <footer>
         <?= \Studip\Button::createAccept($controller->_('Meeting betreten'))?>
