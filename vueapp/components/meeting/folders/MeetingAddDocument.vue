fcefef2062527d6f21444673f4724322d4c4accf
ferishili <zamanifarbod2@gmail.com>
2024-11-04 08:47:37 +0100

    repair styles: file upload

--- vueapp/components/meeting/folders/MeetingAddDocument.vue
+++ vueapp/components/meeting/folders/MeetingAddDocument.vue
@@ -39,13 +39,15 @@
                         </template>
                     </div>
                 </div>
-                <label class="file drag-and-drop meeting-fileupload-dnd" @drop.prevent="getDroppedFile">
-                    {{ $gettext('Neue Datei zum Hinzufügen per Drag & Drop in diesen Bereich ziehen.') }}
-                    <input type="file" name="file" tabeindex="0" :title="tooltip_title_text" ref="meeting_upload_file" @change="getSelecedUploadFile">
-                    <div v-if="filename">
-                        <span>{{ filename }}</span>
-                    </div>
-                </label>
+                <div class="drag-and-drop-wrapper">
+                    <label class="file drag-and-drop meeting-fileupload-dnd" @drop.prevent="getDroppedFile">
+                        {{ $gettext('Neue Datei zum Hinzufügen per Drag & Drop in diesen Bereich ziehen.') }}
+                        <input type="file" name="file" tabeindex="0" :title="tooltip_title_text" ref="meeting_upload_file" @change="getSelecedUploadFile">
+                        <div v-if="filename">
+                            <span>{{ filename }}</span>
+                        </div>
+                    </label>
+                </div>
             </template>
         </studip-dialog>
     </div>
