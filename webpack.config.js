2a88ae74eb8e4843c6d3a0b4b3d9ec4fffaabcef
luniki <meetings.plugin@luniki.de>
2025-06-20 10:20:55 +0200

    Fix build tools.

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,98 @@
-const path = require('path'); // node.js uses CommonJS modules
+const path = require("path");
 
-const { VueLoaderPlugin }       = require('vue-loader');
-const HtmlWebpackPlugin         = require('html-webpack-plugin');
-const { CleanWebpackPlugin }    = require('clean-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
 module.exports = {
-    entry: ['./vueapp/app.js', './assets/css/meetings.scss'], // the entry point
+    entry: ["./vueapp/app.js", "./assets/css/meetings.scss"], // the entry point
     output: {
-        filename: '[name].[contenthash].js', // the output filename
-        path: path.resolve(__dirname, 'static'), // fully qualified path
-        publicPath: '/'
+        filename: "[name].[contenthash].js", // the output filename
+        path: path.resolve(__dirname, "static"), // fully qualified path
+        publicPath: "/",
     },
     module: {
-        rules: [{
-            test: /\.vue$/,
-            use: 'vue-loader'
-        }, {
-			test: /\.scss$/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader
-                },
-                {
-                    loader: "css-loader",
-                    options: {
-                        importLoaders: 2
-                    }
-                },
-                {
-                    loader: 'sass-loader'
-                }
-            ]
-		}, {
-            test: /\.css$/,
-            use: [
-                'vue-style-loader',
-                'css-loader'
-            ]
-        }]
+        rules: [
+            {
+                test: /\.vue$/,
+                use: "vue-loader",
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 2,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ["vue-style-loader", "css-loader"],
+            },
+        ],
     },
     plugins: [
         new CleanWebpackPlugin(),
         new VueLoaderPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'styles.css',
+            filename: "styles.css",
         }),
         new CssMinimizerPlugin({
             minimizerOptions: {
                 discardComments: {
-                    removeAll: true
+                    removeAll: true,
                 },
-                safe: true
-            }
+                safe: true,
+            },
         }),
         new HtmlWebpackPlugin({
-            template: 'vueapp/course_index.php',
+            template: "vueapp/course_index.php",
             inject: false,
             minify: false,
-            filename: '../app/views/index/index.php'
+            filename: "../app/views/index/index.php",
         }),
         new HtmlWebpackPlugin({
-            template: 'vueapp/admin_index.php',
+            template: "vueapp/admin_index.php",
             inject: false,
             minify: false,
-            filename: '../app/views/admin/index.php'
+            filename: "../app/views/admin/index.php",
         }),
         new HtmlWebpackPlugin({
-            template: 'vueapp/lobby_index.php',
+            template: "vueapp/lobby_index.php",
             inject: false,
             minify: false,
-            filename: '../app/views/room/lobby.php'
+            filename: "../app/views/room/lobby.php",
         }),
         new HtmlWebpackPlugin({
-            template: 'vueapp/lobby_index.php',
+            template: "vueapp/lobby_index.php",
             inject: false,
             minify: false,
-            filename: '../app/views/room/qrcode_lobby.php'
+            filename: "../app/views/room/qrcode_lobby.php",
         }),
         new HtmlWebpackPlugin({
-            template: 'vueapp/lobby_index.php',
+            template: "vueapp/lobby_index.php",
             inject: false,
             minify: false,
-            filename: '../app/views/room/public_lobby.php'
+            filename: "../app/views/room/public_lobby.php",
         }),
     ],
     resolve: {
-        extensions: ['.vue', '.js'],
+        extensions: [".vue", ".js"],
         alias: {
-            '@': path.resolve(__dirname, 'vueapp'),
-            '@studip': path.resolve(__dirname, 'vueapp/components/studip'),
-            '@meeting': path.resolve(__dirname, 'vueapp/components/meeting'),
-        }
-    }
+            "@": path.resolve(__dirname, "vueapp"),
+            "@studip": path.resolve(__dirname, "vueapp/components/studip"),
+            "@meeting": path.resolve(__dirname, "vueapp/components/meeting"),
+        },
+    },
 };
