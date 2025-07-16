f6972a7ff8677289b8c6a91900b250f211117bb4
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2025-07-16 10:39:20 +0200

    rework and update logoutUrl and return mechanism, fixes #492

--- app/views/index/return.php
+++ app/views/index/return.php
@@ -1,12 +1,17 @@
-<h4>
+<h4 id="loading">
     <img style="vertical-align: middle; margin-right: 3px;" src="<?= Assets::url('images/ajax-indicator-black.svg') ?>" width="24" height="24" alt="waiting">
     <span><?= $_('Vorgang läuft. Bitte warten Sie einen Moment') ?>&hellip;</span>
 </h4>
+<div id="duplicationInfo" class="hidden" style="display: none;">
+    <?= MessageBox::info(_('Dies ist ein doppelter Tab. Bitte schließen Sie dieses Fenster manuell oder nutzen Sie den Button "Zurück zu den Meetings". Ihr Browser blockiert möglicherweise das automatische Schließen.')) ?>
+    <?= \Studip\LinkButton::create(_('Zurück zu den Meetings'), $return_url)?>
+</div>
 <script type="text/javascript">
     window.onload = () => {
         if (typeof window.page_available !== 'undefined') {
             if (window.page_available) {
-                window.close();
+                document.getElementById('loading').style.display = 'none';
+                document.getElementById('duplicationInfo').style.display = 'block';
             } else {
                 setTimeout(() => {
                     window.open('<?= $return_url ?>', "_self");
